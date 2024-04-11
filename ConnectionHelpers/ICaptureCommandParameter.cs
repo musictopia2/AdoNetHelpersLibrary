0e29e0c9e09ff2b657bc2d5a35ddcee1b05d47b4
Andy Walter <musictopia2@hotmail.com>
2024-04-11 15:52:34 -0700

    Moved the capture command parameter to common functions library so the music core can use it without referencing this library.

--- ConnectionHelpers/ICaptureCommandParameter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AdoNetHelpersLibrary.ConnectionHelpers;
-public interface ICaptureCommandParameter
-{
-    //needs to be public so i can use extensions that use it.
-    DbParameter GetParameter();
-    IDbCommand GetCommand();
-    EnumDatabaseCategory Category { get; }
-    IDbConnection CurrentConnection { get; }
-}
